769f0e96efc26e6660ac617a32fc696cc1cacbb0
Stella Sun <sunshuyu0413@163.com>
2022-08-22 11:44:33 -0400

    set up server.js file & routes folder

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const apiRoutes = require('./routes/api_routes');
+const htmlRoutes = require('./routes/html_routes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}!`);
+});
